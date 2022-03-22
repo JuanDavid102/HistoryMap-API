882117d69c895812ce48f4564e930145d3f2ef83
alumno <alumno@vm1>
2022-03-22 13:52:51 +0100

    migraciones de marcadores y eventos

---
+++ database/migrations/2022_03_22_122306_create_marcadores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarcadoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marcadores', function (Blueprint $table) {
+            $table->id();
+            $table->point("posicion");
+            $table->string("descripcion", 100);
+            $table->string("tipo", 25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marcadores');
+    }
+}
