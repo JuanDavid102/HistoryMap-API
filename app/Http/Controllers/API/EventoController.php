348276460ac851959dd999767bb29475b5e809c0
alumno <alumno@vm1>
2022-03-24 10:32:22 +0100

    Creacion del modelo-controlador-recurso para evento y marcador

---
+++ app/Http/Controllers/API/EventoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Evento;
+use Illuminate\Http\Request;
+
+class EventoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Evento  $evento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Evento $evento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Evento  $evento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Evento $evento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Evento  $evento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Evento $evento)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,31 @@
 <?php
 
+use App\Http\Controllers\API\EventoController;
+use App\Http\Controllers\API\MarcadorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use Psr\Http\Message\ServerRequestInterface;
+use Tqdev\PhpCrudApi\Api;
+use Tqdev\PhpCrudApi\Config;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/eventos");
-Route::get("/marcadores");
+Route::get("eventos", EventoController::class);
+
+Route::get("marcadores", MarcadorController::class);
+
+
+Route::any('/{any}', function (ServerRequestInterface $request) {
+    $config = new Config([
+        'address' => env('DB_HOST', '127.0.0.1'),
+        'username' => env("DB_USERNAME"),
+        'password' => env("DB_PASSWORD"),
+        'database' => env("DB_DATABASE"),
+        'basePath' => '/api',
+    ]);
+    $api = new Api($config);
+    $response = $api->handle($request);
+    return $response;
+})->where('any', '.*');
